71bc57bf021d82eae2b5a56dc63656aecb86d9fb
tim-danger <tim.danger85@gmail.com>
2025-05-16 18:17:36 +0200

    Spiel-Logik in Main-Methode vereinfacht

--- tictactoe-core/src/main/java/org/example/Spiel.java
+++ tictactoe-core/src/main/java/org/example/Spiel.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.swing.SpielStatus;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,17 +17,17 @@ public class Spiel {
     public void spiel() {
 
         // prüfen, ob es einen Gewinn / Unentschieden gibt
-        boolean gewonnen = false;
+        SpielStatus status = getStatus(controller, spielfeld);
 
         // Spielschleife
-        while (!controller.spielZuEnde(spielfeld)) {
+        while (status == SpielStatus.WEITER) {
             System.out.println(aktuellesZeichen + " ist am Zug");
             Integer row = getInput("Gib bitte Zeilennummer ein:");
             Integer col = getInput("Gib bitte Spaltennummer ein:");
 
-            gewonnen = setzeZeichenUndZeichneSpielfeld(row, col);
+            status = setzeZeichenUndZeichneSpielfeld(row, col);
         }
-        System.out.println("Spiel zu Ende! " + (gewonnen ? aktuellesZeichen + " hat gewonnen!" : "Unentschieden"));
+        System.out.println("Spiel zu Ende! " + (status == SpielStatus.GEWONNEN ? aktuellesZeichen + " hat gewonnen!" : "Unentschieden"));
     }
 
     private Zeichen randomZeichen() {
@@ -33,18 +35,30 @@ public class Spiel {
         return Zeichen.values()[i];
     }
 
-    private boolean setzeZeichenUndZeichneSpielfeld(Integer row, Integer col) {
+    private SpielStatus setzeZeichenUndZeichneSpielfeld(Integer row, Integer col) {
         if (spielfeld.setzeZeichen(aktuellesZeichen, row - 1, col - 1)) {
-            boolean gewonnen = controller.gewonnen(spielfeld);
-            if (!gewonnen) {
-                switchAktuellesZeichen();
-            }
             System.out.println(zeichner.zeichneSpielFeld(spielfeld));
-            return gewonnen;
+            SpielStatus status = getStatus(controller, spielfeld);
+            if (status != SpielStatus.GEWONNEN) {
+                switchAktuellesZeichen();
+                return status;
+            }
         } else {
             System.err.println("Das Zeichen an der Stelle " + row + " / " + col + " ist bereits gesetzt! Gib bitte ein Anderes ein!");
         }
-        return false;
+        return getStatus(controller, spielfeld);
+    }
+
+    private SpielStatus getStatus(SpielController controller, Spielfeld spielfeld) {
+        boolean gewonnen = controller.gewonnen(spielfeld);
+        boolean zuEnde = gewonnen || controller.spielZuEnde(spielfeld);
+        if (gewonnen) {
+            return SpielStatus.GEWONNEN;
+        } else if (zuEnde) {
+            return SpielStatus.UNENTSCHIEDEN;
+        } else {
+            return SpielStatus.WEITER;
+        }
     }
 
     private void switchAktuellesZeichen() {
